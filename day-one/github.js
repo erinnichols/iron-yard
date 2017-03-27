eb32f2efc6b28c4590e8285b5e1c9fc82ca0653c
Erin Nichols <erin.nichols@slate.com>
2017-03-27 15:54:30 -0400

    Add livesearch

--- day-one/github.js
+++ day-one/github.js
@@ -30,6 +30,45 @@ class UnorderedList {
   }
 }
 
+class LiveSearch {
+  constructor() {
+    this._input = document.createElement('input');
+  }
+
+  on(name, callback) {
+    this._input.addEventListener(name, function() {
+      callback.call(this);
+    });
+  }
+
+  render() {
+    return this._input;
+  }
+
+}
+
+let liveSearch = new LiveSearch();
+liveSearch.on('input', function() {
+  console.log(this.value);
+  if (this.value.length >= 2) {
+    // do this stupid search thing
+    new Ajax('https://api.github.com/search/repositories?q=' + this.value)
+      .then(function(text) {
+        let data = JSON.parse(text);
+        let ul = new UnorderedList();
+        data.items.forEach(function(repo) {
+          let li = new ListItem();
+          li.text = repo.name;
+          ul.addItem(li);
+        });
+        document.body.replaceChild(ul.render(), document.body.lastChild);
+      })
+      .get();
+  }
+});
+
+document.body.appendChild(liveSearch.render());
+
 new Ajax('https://api.github.com/repositories')
   .then(function(text) {
     let data = JSON.parse(text);
